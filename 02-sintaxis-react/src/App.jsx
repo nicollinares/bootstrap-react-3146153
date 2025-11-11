1fef13c6a32267617bb60cb761164dc54018a541
nicollinares <linaresgomeznicol@gmail.com>
2025-11-11 13:17:18 -0500

    sección card terminada

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -8,69 +8,82 @@ import { LoginForm } from './components/LoginForm'
 export const App = () => {
   return (
     <>
-    <NavBar/>
-    <div className="containerCards">
-      <ExtensionCard 
-      cardTitle="DevLens" 
-      cardImage="./public/imagen 1.png" 
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Quickly inspect page layouts and wisualize element boundaries"
-      />
-      <ExtensionCard 
-      cardTitle="StyleSpy" 
-      cardImage="./public/imagen 2.png" 
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Instantly analyze and copy CSS from any webpage element"
-      />
-      <ExtensionCard 
-      cardTitle="SpeedBoost" 
-      cardImage="./public/imagen 3.png" 
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Optimizes browser resource usage to accelerate page loading"
-      />
-      <ExtensionCard 
-      cardTitle="JSONWizard" 
-      cardImage="./public/imagen 4.png" 
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Formats, validates, and prettifies JSON responses in-browser"
-      />
-      <ExtensionCard 
-      cardTitle="TahMaster Pro" 
-      cardImage="./public/imagen 5.png"  
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Organizes browser tabs into groups and sessions"
-      />
-      <ExtensionCard 
-      cardTitle="ViewportBuddy" 
-      cardImage="./public/imagen 6.png"  
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Simulates various screen resolutions directly within the browser"
-      />
-      <ExtensionCard 
-      cardTitle="Markun Notes" 
-      cardImage="./public/imagen 7.png"  
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Enables annotation and notes directly onto webpages for collaborative debugging"
-      />
-      <ExtensionCard 
-      cardTitle="GridGuides" 
-      cardImage="./public/imagen 8.png"  
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="Overlay customizable grids and alignment guides on any webpage."
-      />
-      <ExtensionCard 
-      cardTitle="Palette Picker" 
-      cardImage="./public/imagen 9.png" 
-      cardImagen= "./public/imagen 10.png"
-      cardDescription="20 Instantly extract color pareste strany webpage."
-      />
+      <NavBar />
+      <div className="containerCards">
+        <ExtensionCard
+          cardTitle="DevLens"
+          cardImage="./public/imagen 1.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Quickly inspect page layouts and wisualize element boundaries"
+        />
+        <ExtensionCard
+          cardTitle="StyleSpy"
+          cardImage="./public/imagen 2.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Instantly analyze and copy CSS from any webpage element"
+        />
+        <ExtensionCard
+          cardTitle="SpeedBoost"
+          cardImage="./public/imagen 3.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Optimizes browser resource usage to accelerate page loading"
+        />
+        <ExtensionCard
+          cardTitle="JSONWizard"
+          cardImage="./public/imagen 4.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Formats, validates, and prettifies JSON responses in-browser"
+        />
+        <ExtensionCard
+          cardTitle="TahMaster Pro"
+          cardImage="./public/imagen 5.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Organizes browser tabs into groups and sessions"
+        />
+        <ExtensionCard
+          cardTitle="ViewportBuddy"
+          cardImage="./public/imagen 6.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Simulates various screen resolutions directly within the browser"
+        />
+        <ExtensionCard
+          cardTitle="Markun Notes"
+          cardImage="./public/imagen 7.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Enables annotation and notes directly onto webpages for collaborative debugging"
+        />
+        <ExtensionCard
+          cardTitle="GridGuides"
+          cardImage="./public/imagen 8.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="Overlay customizable grids and alignment guides on any webpage."
+        />
+        <ExtensionCard
+          cardTitle="Palette Picker"
+          cardImage="./public/imagen 9.png"
+          cardImagen="./public/imagen 10.png"
+          cardDescription="20 Instantly extract color pareste strany webpage."
+        />
 
-    </div>
-    <h1>React + Bootstrap</h1>
-    <LoginForm/>
+      </div>
+      <h1>React + Bootstrap</h1>
+      <LoginForm />
 
-    <ProductCard/>
-    
+      <div className="container" >
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4 " >
+            <ProductCard />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 mt-md-0" >
+            <ProductCard />
+          </div>
+          <div className="col-12 col-md-12 col-lg-4 mt-4 mt-lg-0" >
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+
+
     </>
   )
 }
--- 02-sintaxis-react/src/components/ProductCard.jsx
+++ 02-sintaxis-react/src/components/ProductCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const ProductCard = () => {
     return (
-        <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..."/>
+        <div className="card">
+           <div className="bg-secondary rounded-circle m-auto mt-3" style={{width: "80px", height: "80px"}}></div>
                 <div className="card-body text-center">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
