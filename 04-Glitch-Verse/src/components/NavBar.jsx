a36101785a42dea86ce1447ea9d464b4f9fde6b8
nicollinares <linaresgomeznicol@gmail.com>
2025-11-25 14:29:36 -0500

    Avance

--- 04-Glitch-Verse/src/components/NavBar.jsx
+++ 04-Glitch-Verse/src/components/NavBar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./NavBar.css";
 import "../Styles/NavBar.css"
 
-export const NavBar = () => {
+export const Navbar = () => {
   return (
-    <div>NavBar</div>
-  )
+    <header className="navbar-container">
+      <div className="navbar-content container d-flex justify-content-between align-items-center">
+
+        {/* --- IZQUIERDA --- */}
+        <div className="nav-left d-flex align-items-center gap-4">
+          <span className="nav-link-custom">Conoce el cómic</span>
+        </div>
+
+        {/* --- CENTRO (LOGO + BOTÓN) --- */}
+        <div className="nav-center d-flex flex-column align-items-center">
+          <img
+            src="/logo.png"
+            alt="GlitchVerse"
+            className="logo-nav"
+          />
+          <button className="btn btn-nav mt-1">Conoce GlitchVerse</button>
+        </div>
+
+        {/* --- DERECHA (ICONOS) --- */}
+        <div className="nav-right d-flex align-items-center gap-4">
+          <i className="bi bi-search nav-icon"></i>
+          <i className="bi bi-cart nav-icon"></i>
+        </div>
+
+      </div>
+    </header>
+  );
 }
