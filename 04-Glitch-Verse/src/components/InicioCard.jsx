f5361e790b28f98682a506ad6af45c82352f6d2f
nicollinares <linaresgomeznicol@gmail.com>
2025-12-03 13:26:01 -0500

    avanceeee

--- 04-Glitch-Verse/src/components/InicioCard.jsx
+++ 04-Glitch-Verse/src/components/InicioCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { NavBar } from './NavBar'
 
 
-   export const InicioCard = () => {
-     return (
+export const InicioCard = () => {
+  return (
 
-   <div className="container inicio-container text-center mt-5 mb-5">
+    <div className="container inicio-container text-center mt-5 mb-5">
       <h3 className="inicio-title">Conoce GlitchVerse</h3>
 
       <div className="d-flex justify-content-center align-items-center mt-3">
@@ -13,17 +13,59 @@ import { NavBar } from './NavBar'
         <div className="linea"></div>
 
         {/* Texto principal */}
-        <p className="inicio-text mx-4">
+        <h1 className="inicio-text">
           Nuestra marca se enfoca en busca la mejoría en el trato que le
           podemos dar a la tecnología, buscando a través de un cómic interactivo,
           mostrando la importancia que tiene el buen manejo de las redes
           sociales y la tecnología.
-        </p>
+        </h1>
 
         {/* Línea derecha */}
         <div className="linea"></div>
       </div>
+
+
+
+
+
+      <div className="inicio-section d-flex justify-content-center align-items-center">
+        <div className="container row align-items-center g-4">
+
+          {/* --- TEXTO IZQUIERDA --- */}
+          <div className="col-md-7 text-center text-md-start">
+            <button className="btn-titulo mb-4">
+              ¿Cómo nació nuestra marca?
+            </button>
+
+            <p className="texto-descripcion">
+              Glitchverse nació de la necesidad de contar historias que advirtieran
+              sobre los riesgos del mal uso tecnológico. Somos una empresa creativa
+              que desarrolla cómics interactivos totalmente distópicos, donde cada decisión
+              del lector revela las consecuencias de vivir en un mundo dominado por lo digital.
+              No solo buscamos entretener, sino generar conciencia sobre el impacto real de
+              nuestras elecciones en un futuro cada vez más cercano.
+            </p>
+          </div>
+
+          {/* --- IMAGEN DERECHA --- */}
+          <div className="col-md-5 text-center">
+            <img
+              src="/img/ilustracion.png"
+              className="img-fluid img-ilustracion"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-     )
-   }
-   
+
+
+
+
+
+
+
+
+
+
+  )
+}
--- 04-Glitch-Verse/src/components/NavBar.jsx
+++ 04-Glitch-Verse/src/components/NavBar.jsx
@@ -29,44 +29,6 @@ export const NavBar = () => {
       </nav>
 
 
-
-
-      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm py-3">
-        <div className="container">
-          <a className="navbar-brand fw-bold glitch-title" href="/">
-            GlitchVerse
-          </a>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#inicio">Inicio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">Sobre Nosotros</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#comic">Cómic</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contacto">Contacto</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-
-
-
     </header>
   )
 }
